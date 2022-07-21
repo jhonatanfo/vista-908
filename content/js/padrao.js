53cf0f3ec6f5e9a6401ecd22dfbcc940159ba38b
jhonatan <jnatanfo@hotmail.com>
2022-07-20 23:18:02 -0300

    feat: versão 3

--- content/js/padrao.js
+++ content/js/padrao.js
@@ -1,45 +1,15 @@
 $(document).on('ready', function() {
-
-    var o = $(".princial-banner");
-    $(document).ready(function() {
-        o.slick({
-            arrows: false,
-            dots: true,
-            infinite: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 500,
-            fade: true,
-            customPaging: function(slider, i) {
-                var thumb = jQuery(slider.$slides[i]).data();
-                return '<a>0' + (i + 1) + '</a>';
-            },
-            cssEase: 'linear'
-        }), o.slickAnimation()
-    });
-
-    $('.slick-ultimas-noticias').slick({
-        slidesToShow: 1,
+    $('.slick-galeria-fotos').slick({
+        slidesToShow: 3,
         slidesToScroll: 1,
-        arrows: true,
-        dots: false,
-        fade: true,
+        centerMode: true,
+        focusOnSelect: true,
+        arrow: true,
+        infinite: true,
     });
-
-    $('.slick-depoimentos').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        dots: true,
-        fade: true,
-    });
-
-
-
 });
 
 $(document).ready(function() {
-
     $(".navbar-nav li a").click(function() {
         $(".navbar-collapse.collapse").removeClass("in");
     });
@@ -53,19 +23,12 @@ $(document).ready(function() {
             scrollTop: targetOffset - 105
         }, 800);
     });
-
-
-
-
 });
 
 $(document).on("scroll", function() {
     if ($(document).scrollTop() > 105) { //QUANDO O SCROLL PASSAR DOS 100px DO TOPO
-
         $(".navbar").removeClass("large").addClass("small"); //TROCA P CLASSE MENOR
     } else {
-
-
         $(".navbar").removeClass("small").addClass("large"); //VOLTA P MENU GRANDE ORIGINAL
     }
 });
